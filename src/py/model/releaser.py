b77c0fcdc03e557a9d6721353a3891a85a66878c
Alexander Lepe <aleksanderlepe@gmail.com>
2019-05-19 04:26:27 +0300

    Files of latest release, model releaser

--- src/py/model/releaser.py
+++ src/py/model/releaser.py
@@ -1,6 +1,7 @@
 import os
 import pickle
 import logging
+import numpy as np
 from shutil import copyfile
 from model import RNN
 from utils import config
@@ -12,6 +13,7 @@ from tester import Tester
 class Releaser:
     def __init__(self):
         self.config = config()
+        self.dataset_path = self.config['dict_path']
         self.model_key = self.config['model_key']
         self.chars = self.config['chars']
         self.gram_types = self.config['grammemes_types']
@@ -26,13 +28,22 @@ class Releaser:
         ]
         self.xml_gram_paths = [
             os.path.join(path, "grams.xml")
-            for path in self.config['publish_gramms_path']
+            for path in self.config['publish_gramm_paths']
         ]
         self.test_result_paths = [
             os.path.join(path, "test_info.txt")
             for path in self.config['test_results_paths']
         ]
+        self.publish_dataset_info_paths = [
+            os.path.join(path, "dataset_info.txt")
+            for path in self.config['publish_dataset_info_paths']
+        ]
+        self.tests_results_paths = self.config['publish_test_paths']
         self.classes_dic = self.config['main_classes']
+        self.rev_classes_dic = {
+            self.classes_dic[key]: ",".join([key for key in list(key) if key is not None])
+            for key in self.classes_dic
+        }
 
 
     def release_model(self):
@@ -40,10 +51,12 @@ class Releaser:
         for path in self.pd_publish_paths:
             copyfile(pd_release_path, path)
 
+        self.__release_test_files__()
+        self.__release_dataset_info__()
         self.__release_model_xml__(out_ops, gram_ops)
         self.__release_grams_xml__()
         tester = Tester()
-        self.__build_test_results(tester)
+        self.__release_test_results__(tester)
         self.__build_bad_words__(tester)
 
 
@@ -127,7 +140,20 @@ class Releaser:
             with open(path, 'wb+') as f:
                 tree.write(f, xml_declaration=True, encoding='utf-8')
 
-    def __build_test_results(self, tester):
+    def __release_dataset_info__(self):
+        doc = etree.iterparse(self.dataset_path, events=('start', 'end'))
+        itr = iter(doc)
+        event, element = next(itr)
+        while not (event == 'start' and element.tag == 'dictionary'):
+            pass
+
+        version = element.attrib['version']
+        revision = element.attrib['revision']
+        for path in self.publish_dataset_info_paths:
+            with open(path, 'w+') as f:
+                f.write(f"dictionary\nversion={version}\nrevision={revision}")
+
+    def __release_test_results__(self, tester):
         results = tester.test()
         for path in self.test_result_paths:
             with open(path, 'w+') as f:
@@ -167,6 +193,55 @@ class Releaser:
         print(f"Wrong lemmas count: {len(wrong_words)}")
         with open(os.path.join("wrong_words.pkl"), 'wb+') as f:
             pickle.dump(wrong_words, f)
+
+    def __release_test_files__(self):
+        for gram in self.gram_types:
+            cls = self.gram_types[gram]['classes']
+            dic = {cls[g_key]['index']: g_key for g_key in cls}
+            self.__release_cls_tests__(gram, dic)
+
+        self.__release_cls_tests__('main', self.rev_classes_dic)
+        self.__release_lemma_tests__()
+
+    def __release_cls_tests__(self, key, cls_dic):
+        path = os.path.join(self.config['dataset_path'], f"{key}_test_dataset.pkl")
+        with open(path, 'rb') as f:
+            words = pickle.load(f)
+
+        root = etree.Element('Tests')
+        for word in words:
+            y = np.argwhere(word['y'] == 1).ravel()
+            y = ';'.join([cls_dic[index] for index in y])
+            test = etree.Element("T")
+            test.set('x', word['src'])
+            test.set('y', y)
+            root.append(test)
+
+        for dir_path in self.tests_results_paths:
+            rez_path = os.path.join(dir_path, f'{key}.xml')
+            tree = ElementTree(root)
+            with open(rez_path, 'wb+') as f:
+                tree.write(f, xml_declaration=True, encoding='utf-8')
+
+    def __release_lemma_tests__(self):
+        path = os.path.join(self.config['dataset_path'], "lemma_test_dataset.pkl")
+        with open(path, 'rb') as f:
+            words = pickle.load(f)
+
+        root = etree.Element('Tests')
+        for word in words:
+            test = etree.Element("T")
+            test.set('x', word['x_src'])
+            test.set('y', word['y_src'])
+            root.append(test)
+
+        for dir_path in self.tests_results_paths:
+            rez_path = os.path.join(dir_path, 'lem.xml')
+            tree = ElementTree(root)
+            with open(rez_path, 'wb+') as f:
+                tree.write(f, xml_declaration=True, encoding='utf-8')
+
+
 
 
 if __name__ == "__main__":
